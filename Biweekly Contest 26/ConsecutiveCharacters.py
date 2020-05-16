403eed54f93bfe0fb2c621b17d7fec108966cf57
harshavaranasi61 <39255630+harshavaranasi61@users.noreply.github.com>
2020-05-16 23:20:27 +0530

    Create ConsecutiveCharacters.py

---
+++ Biweekly Contest 26/ConsecutiveCharacters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        ans, temp = 1, 1
+        for i in range(1, len(s)):
+            if (s[i] == s[i - 1]): 
+                temp += 1
+            else: 
+                ans = max(ans, temp) 
+                temp = 1
+        ans = max(ans, temp) 
+        return ans 
